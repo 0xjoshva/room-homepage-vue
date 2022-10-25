93f25902f2ad18ccf7e34b777193550ecdd058b7
0xjoshva <joshuasteed123@gmail.com>
2022-10-25 21:34:42 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;600;700&display=swap');
+/* font-family: 'League Spartan', sans-serif; */
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html, body {
+  height: 100%;
+  font-size: 12px;
+}
+body {
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
 }
 
-nav {
-  padding: 30px;
-}
+:root{
+  --darkgrey: hsl(0, 0%, 63%);
+  --verydarkgrey: hsl(0, 0%, 27%);
+  --black: hsl(0, 0%, 0%);
+  --white: hsl(0, 0%, 100%);
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,207 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <nav>
+      <img src="../assets/logo.svg" alt="" id="logo" />
+      <div class="router-links">
+        <router-link to="/" class="router-link">home</router-link>
+        <router-link to="/" class="router-link">shop</router-link>
+        <router-link to="/" class="router-link">about</router-link>
+        <router-link to="/" class="router-link">contact</router-link>
+      </div>
+    </nav>
+    <div class="container">
+      <img src="../assets/desktop-image-hero-1.jpg" alt="" id="carousel-img" />
+      <div class="wrapper">
+        <div class="text">
+          <h2>Discover innovative ways to decorate</h2>
+          <p>
+            We provide unmateched quality, comfort, and style for property
+            owners across the country. Our expert combine form and function in
+            bringing your vision to life. Create a room in your own style with
+            our collection and make your property a reflection of you and what
+            you love.
+          </p>
+          <button>
+            SHOP NOW
+            <img src="../assets/icon-arrow.svg" alt="" class="shop-arrow" />
+          </button>
+        </div>
+        <div class="carousel-buttons">
+          <div class="arrows">
+            <img src="../assets/icon-angle-left.svg" alt="" />
+          </div>
+          <div class="arrows">
+            <img src="../assets/icon-angle-right.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <img src="../assets/image-about-dark.jpg" alt="" />
+      <div class="about">
+        <h6>ABOUT OUR FURNITURE</h6>
+        <p>
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme at
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </div>
+      <img src="../assets/image-about-light.jpg" alt="" />
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background: var(--white);
+  font-family: "League Spartan", sans-serif;
+}
+nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 3;
+  display: flex;
+  column-gap: 5rem;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  padding-top: 6rem;
+  padding-left: 6rem;
+}
+nav img {
+  min-height: 20px;
+  width: auto;
+}
+.router-links {
+  display: flex;
+  flex-direction: row;
+  column-gap: 2rem;
+}
+.router-link {
+  font-size: 18px;
+  text-decoration: none;
+  color: var(--white);
+}
+.container {
+  display: flex;
+  width: 100%;
+  height: 53rem;
+  min-height: 53rem;
+}
+#carousel-img {
+  min-width: 1200px;
+  width: 1200px;
+  min-height: 10rem;
+  object-fit: fill;
+}
+.text {
+  padding-top: 10rem;
+  width: fit-content;
+  height: 100%;
+  padding-bottom: 0;
+}
+.text h2 {
+  font-size: 5rem;
+  line-height: 5rem;
+  font-weight: 600;
+  padding-bottom: 2rem;
+  margin: 0 auto;
+  width: 70%;
+}
+.text p {
+  font-size: 18px;
+  color: var(--darkgrey);
+  margin: 0 auto;
+  width: 70%;
+  text-align: justify;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.carousel-buttons {
+  background: var(--white);
+  width: 100%;
+  max-height: 10rem;
+  height: 10rem;
+  display: flex;
+}
+.arrows {
+  background: var(--black);
+  width: fit-content;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 3.5rem;
+  padding-right: 3.5rem;
+  transition: 0.2s all cubic-bezier(0.46, 0.03, 0.52, 0.96);
+}
+.arrows:hover {
+  background: var(--verydarkgrey);
+}
+.wrapper button {
+  width: 70%;
+  height: fit-content;
+  background: var(--white);
+  border: none;
+  font-size: 1.6rem;
+  letter-spacing: 9px;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  font-weight: 600;
+  padding: 0;
+  margin-top: 2rem;
+  column-gap: 0px;
+  transition: 0.4s all cubic-bezier(0.68, -0.55, 0.27, 1.55);
+}
+.wrapper button:hover {
+  column-gap: 10px;
+  letter-spacing: 10px;
+}
+.shop-arrow {
+  width: 2rem;
+  height: auto;
+  width: fit-content;
+  height: fit-content;
+  margin-left: 2rem;
+}
+.bottom {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.bottom img {
+  height: 27.75rem;
+  width: 43.6rem;
+  object-fit: cover;
+}
+.about {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem;
+}
+.about h6{
+font-size: 2rem;
+letter-spacing: 8px;
+width: 75%;
+
+}
+.about p{
+  font-size: 18px;
+  color: var(--darkgrey);
+  width: 75%;
+}
+</style>
